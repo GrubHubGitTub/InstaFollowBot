951fdb888e2b64148329db0b098832526215254b
GrubHubGitTub <aidangruber@hotmail.com>
2022-02-23 12:23:19 +0700

    update for public release

--- main.py
+++ main.py
@@ -1,79 +1,115 @@
 import random
-
-import selenium as s
+import os
+import subprocess
+import sys
+def install(package):
+    subprocess.check_call([sys.executable, "-m", "pip", "install", package])
+install("selenium")
+from selenium import webdriver
+from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from time import sleep
+install("python-dotenv")
+from dotenv import load_dotenv
 
-email = ""
-pw = ""
+# _______ CHANGE PATH TO YOUR ENV FILE IF NECESSARY________
+load_dotenv(".env")
+email = os.getenv("email")
+pw = os.getenv("pw")
 
-login_choice = input("Sign in through ig or fb?")
+#   -----------CHOOSE WEBDRIVER (REMOVE HEADLESS OPTION TO SEE SELENIUM RUNNING)------------
+install("webdriver-manager")
+
+# # CHROME DRIVER USE THIS:
+# from selenium.webdriver.chrome.service import Service
+# from webdriver_manager.chrome import ChromeDriverManager
+# from selenium.webdriver.chrome.options import Options
+# options = Options()
+# options.headless = True
+# driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+
+# EDGE DRIVER USES THIS:
+from selenium.webdriver.edge.service import Service
+from webdriver_manager.microsoft import EdgeChromiumDriverManager
+from selenium.webdriver.edge.options import Options
+options = Options()
+options.add_argument("headless")
+driver = webdriver.Edge(service = Service(EdgeChromiumDriverManager().install()), options=options)
+
+
+def login():
+    login_choice = input("Sign in through ig or fb?")
+
+    # login through insta
+    if login_choice == "ig":
+        driver.get("https://www.instagram.com")
+        sleep(3)
+        print("Logging in to instagram...")
+        username_input = driver.find_element(By.NAME, "username")
+        username_input.send_keys(email)
+        pw_input = driver.find_element(By.NAME, "password")
+        pw_input.send_keys(pw)
+        login_button = driver.find_element(By.CSS_SELECTOR, ".sqdOP.L3NKy.y3zKF")
+        login_button.click()
+        sleep(5)
+
+    # login through fb
+    if login_choice == "fb":
+        driver.get("https://www.instagram.com")
+        sleep(3)
+        print("Logging in to instagram via Facebook...")
+        current_url = driver.current_url
+        login_with_fb = driver.find_element(By.CSS_SELECTOR,".KPnG0")
+        login_with_fb.click()
+        WebDriverWait(driver, 15).until(EC.url_changes(current_url))
+        fb_email_input = driver.find_element(By.NAME,"email")
+        fb_pw_input = driver.find_element(By.NAME,"pass")
+        fb_email_input.send_keys(email)
+        fb_pw_input.send_keys(pw)
+        login_fb_button = driver.find_element(By.NAME,"login")
+        login_fb_button.send_keys(Keys.ENTER)
+        sleep(10)
+        # no_notif = driver.find_element(By.CSS_SELECTOR,".aOOlW.HoLwm")
+        # no_notif.click()
+        # sleep(3)
+
+def get_account(account):
+    print(f"Looking for {account}")
+    user_search = driver.find_element(By.CSS_SELECTOR, ".XTCLo")
+    user_search.send_keys(account)
+    sleep(2)
+    user = driver.find_element(By.CSS_SELECTOR, ".-qQT3")
+    user.click()
+    sleep(7)
+
+def get_followers():
+    show_followers = driver.find_element(By.CSS_SELECTOR, "ul.k9GMp li:nth-child(2) a")
+    show_followers.click()
+    sleep(3)
+    follow_buttons = driver.find_elements(By.CSS_SELECTOR,".sqdOP.L3NKy.y3zKF")
+    follower_amount = len(follow_buttons)
+    while follower_amount < 50:
+        popup = driver.find_element(By.XPATH, "/html/body/div[6]/div//a")
+        popup.send_keys(Keys.END)
+        follow_buttons = driver.find_elements(By.CSS_SELECTOR,".sqdOP.L3NKy.y3zKF")
+        follower_amount = (len(follow_buttons))
+        print(f" Collecting followers: {follower_amount}")
+        sleep(1)
+    follow(follow_buttons)
+
+def follow(follow_buttons):
+    count = 1
+    for button in follow_buttons:
+        rand_sleep = random.randint(1, 5)
+        button.click()
+        print(f"Clicking {count}")
+        sleep(rand_sleep)
+        count += 1
+
 account = input("From what account would you like to get followers?")
-
-# login through insta
-if login_choice == "ig":
-    chrome_driver = "C:/Program Files (x86)/chromedriver_win32/chromedriver.exe"
-    driver = s.webdriver.Chrome(executable_path=chrome_driver)
-    driver.get("https://www.instagram.com")
-    sleep(3)
-    username_input = driver.find_element_by_name("username")
-    username_input.send_keys(email)
-    pw_input = driver.find_element_by_name("password")
-    pw_input.send_keys(pw)
-    login_button = driver.find_element_by_css_selector(".sqdOP.L3NKy.y3zKF")
-    login_button.click()
-    sleep(5)
-
-# login through fb
-if login_choice == "fb":
-    chrome_driver = "C:/Program Files (x86)/chromedriver_win32/chromedriver.exe"
-    driver = s.webdriver.Chrome(executable_path=chrome_driver)
-    driver.get("https://www.instagram.com")
-    sleep(3)
-    current_url = driver.current_url
-    login_with_fb = driver.find_element_by_css_selector(".KPnG0")
-    login_with_fb.click()
-    WebDriverWait(driver, 15).until(EC.url_changes(current_url))
-    fb_email_input = driver.find_element_by_name("email")
-    fb_pw_input = driver.find_element_by_name("pass")
-    fb_email_input.send_keys(email)
-    fb_pw_input.send_keys(pw)
-    login_fb_button = driver.find_element_by_name("login")
-    login_fb_button.send_keys(Keys.ENTER)
-    sleep(10)
-    no_notif = driver.find_element_by_css_selector(".aOOlW.HoLwm")
-    no_notif.click()
-    sleep(3)
-
-# choose the insta account
-user_search = driver.find_element_by_css_selector(".XTCLo")
-user_search.send_keys(account)
-sleep(2)
-account = driver.find_element_by_css_selector(".-qQT3")
-account.click()
-sleep(5)
-show_followers = driver.find_element_by_css_selector("a.-nal3")
-show_followers.click()
-sleep(3)
-
-# collect 50 buttons
-follow_buttons = driver.find_elements_by_css_selector(".sqdOP.L3NKy.y3zKF")
-follower_amount = len(follow_buttons)
-while follower_amount < 50:
-    popup = driver.find_element_by_xpath("/html/body/div[6]/div//a")
-    popup.send_keys(Keys.END)
-    follow_buttons = driver.find_elements_by_css_selector(".sqdOP.L3NKy.y3zKF")
-    follower_amount = (len(follow_buttons))
-    print(follower_amount)
-    sleep(1)
-
-# click the buttons (insta allows max 50)
-for button in follow_buttons:
-    rand_sleep = random.randint(1, 5)
-    button.click()
-    sleep(rand_sleep)
-
-
+login()
+get_account(account)
+get_followers()
 
--- readme.md
+++ readme.md
@@ -1,7 +1,21 @@
-#Must install Selenium Webdriver, and direct the path to where it is installed. 
-(https://chromedriver.chromium.org/downloads)
+# InstaFollowBot
 
-Add your insta user/pw to the fields email and pw. 
+###Grow your business by following the followers of a specified instagram account.
 
-Run main.py after installing Selenium and adding your information. It will take about 3 minutes to collect and follow all buttons. 
-Instagram only allows around 50 buttons pressed before kicking you from the account. 
+# Installation instructions
+
+Choose your webdriver by commenting out the one you do not need. (Pre configured for Edge and Chrome drivers).
+
+Create a .env file with:
+
+- email= "yourinstaemail@gmail.com"
+- pw= "password"
+
+
+## Run main.py. 
+- ### When prompted, enter the account you would like to get followers from. You can enter a search term and it will get the top result.
+- ### Choose to log in via Facebook or Instagram (type ig or fb in the console when prompted)
+
+
+####Due to instagram not liking bots, the app will follow 50 during one process. Run again if you want more followers, but you may be stopped from following after a certain amount of time. 
+##### If you get some errors, the app may run better if you remove the headless option in the webdriver options. 
